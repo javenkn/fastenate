0ea2bd21ff5b36ccfafdfb261fb9b135f908356f
javenkn <javenkn7@yahoo.com>
2016-06-17 23:35:58 -1000

    added some title divs and topic divs

--- public/app.js
+++ public/app.js
@@ -1,7 +1,36 @@
-$('<div/>').addClass('TopicDiv').append('body');
+var $titleElement = $('<div>');
+$($titleElement).attr('id','Title');
+$('body').append($titleElement);
+
+var $topicElement = $('<div>');
+$($topicElement).addClass('TopicDiv');
+$('body').append($topicElement);
+
 var $getTheApp = $('<div>');
+$($getTheApp).attr('id','get_the_app');
 var $myBoards = $('<div>');
+$($myBoards).attr('id','my_boards');
 var $random = $('<div>');
+$($random).attr('id','random');
 $('.TopicDiv').append($getTheApp);
 $('.TopicDiv').append($myBoards);
 $('.TopicDiv').append($random);
+$($getTheApp).on('click', getData);
+$($myBoards).on('click', getData);
+$($random).on('click', getData);
+
+function getData(event){
+  $.ajax({
+    method: 'GET',
+    url: '/public/api/' + $(this).attr('id') + '.json',
+    dataType: 'json',
+  })
+  .done(function(result) {
+    displayPage(result);
+  });
+
+}
+
+function displayPage(response){
+
+}
